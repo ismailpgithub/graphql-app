2f013476950e9a1907a40222610a7b717f3721fb
ismailpgithub <ismail.p1721@gmail.com>
2026-05-02 20:10:43 +0530

    Added Signin and Signup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
-import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
-import { prismaClient } from './lib/db.js';
+import createApolloGraphqlServer from './graphql/index.js';
 
 async function init() {
     
@@ -9,42 +8,7 @@ const app = express()
 const PORT = Number(process.env.PORT) || 8000
 app.use(express.json())
 
-const gqlServer = new ApolloServer({
-    typeDefs: `
-        type Query {
-            hello: String
-            say(name: String): String 
-        }
-        type Mutation {
-            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
-        }
-    `,
-    resolvers: {
-        Query: {
-            hello: () => `Hey there, I am a graphql server`,
-            say: (_, {name}: {name: String}) => `Hey ${name}, How are you?`
-        },
-        Mutation: {
-            createUser: async (_, 
-                {firstName, lastName, email, password} : 
-                {firstName: string; lastName: string; email: string; password: string}) => {
-                    await prismaClient.user.create({
-                        data:{
-                            email,
-                            firstName,
-                            lastName,
-                            password,
-                            salt: "random_salt"
-                        }
-                    })
-                    return true;
-                }
-        }
-    }
-})
-
-await gqlServer.start()
-
+const gqlServer = await  createApolloGraphqlServer()
 
 app.get('/', (req, res) =>{
 res.json({message: 'Server is up and running '})
